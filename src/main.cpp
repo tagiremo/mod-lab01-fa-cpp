6e88bb1a233c9bcaa2db34b02e70a9125bdf3333
tagiremo <123821344+tagiremo@users.noreply.github.com>
2026-03-29 23:56:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
     const char* str = "Hello world123 tagir slovo sharipov9423423";
-
     std::cout << faStr1(str) << "\n";
     std::cout << faStr2(str) << "\n";
     std::cout << faStr3(str) << "\n";
     return 0;
 }
+
